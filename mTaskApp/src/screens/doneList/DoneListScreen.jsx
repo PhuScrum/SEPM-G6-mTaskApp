e0a536a7f5e133981100d1393c7d0b88ecd6f295
LeePingShan <peter9b289b28@yahoo.com.tw>
2020-04-07 16:04:00 +0700

    Print out the tasks from backend
    Complete fetching data from the server

--- mTaskApp/src/screens/doneList/DoneListScreen.jsx
+++ mTaskApp/src/screens/doneList/DoneListScreen.jsx
@@ -1,15 +1,81 @@
 import React, { Component } from 'react'
 import {Layout, Text } from '@ui-kitten/components';
 import TopNavigationBarBackButton from '../../components/cores/TopNavigationBarBackButton'
-export default class DoneListScreen extends Component {
-    render() {
-        return (
-            <Layout  style={{paddingTop: 16, paddingBottom: 0 }}>
-                <TopNavigationBarBackButton {...this.props} title='Done List'/>
-                <Text category='h1'>Done</Text>
+import {FlatList, StyleSheet, View, ActivityIndicator, TouchableOpacity} from 'react-native';
 
-              
-            </Layout>
+
+export default class DoneListScreen extends Component {
+
+   constructor() {
+       super();
+       this.state = {
+        isLoading: true,
+        dataSource: []
+       }
+   }
+
+   componentDidMount () {
+       return fetch('http://192.168.0.10:19000/task')
+        .then((response) => response.json())
+        .then((responseJson) => {
+            this.setState({
+            isLoading: false,
+            dataSource: responseJson
+        })
+
+       })
+
+      
+   }
+
+    //fetchData = async () => {
+    //    const response = await fetch("http://localhost:19000/task");
+    //    const json =await response.json();
+    //       this.setState({data: json.task});
+
+
+   // }
+
+   _renderItem = ({ item }) => (
+    <TouchableOpacity onPress={() => alert(item.completed)}>
+        <View style={styles.item}> 
+            <Text>{item.name}</Text> 
+        </View>
+
+    </TouchableOpacity>
+   );
+
+    render() {
+
+      
+        
+        return (
+            
+            <View  style={{paddingTop: 20, paddingBottom: 0,  }}>
+                <View style={{marginVertical: 10, borderBottomColor:'black'}}>
+                <TopNavigationBarBackButton {...this.props} title='Done List'/>
+                </View>
+                <Text category='h1'>Done</Text>
+                <FlatList 
+                    data={this.state.dataSource}
+                    renderItem={this._renderItem}
+                    keyExtractor={(item, i) => i}
+                    />
+                
+            </View>
+            
         )
     }
 }
+
+const styles = StyleSheet.create({
+
+    item:{
+        padding: 5,
+        borderBottomWidth: 1,
+        borderBottomColor: '#eee'
+
+
+
+    },
+});
