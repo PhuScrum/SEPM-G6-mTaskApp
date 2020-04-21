fda653259e6eeab89ce588c9d86e87fbf97caad4
Anh Tu Bui <40556943+anhtubui1204@users.noreply.github.com>
2020-04-21 16:41:15 +0700

    hi

--- mTaskApp/src/components/tasks/AddTask.js
+++ mTaskApp/src/components/tasks/AddTask.js
@@ -3,7 +3,10 @@ import { StyleSheet, View, TouchableOpacity,} from 'react-native'
 import { Layout, Text, Input, Button, Datepicker, Icon } from '@ui-kitten/components';
 import moment from 'moment-timezone'
 import DateTimePicker from '@react-native-community/datetimepicker';
-import CustomDateTimePicker from './CustomDateTimePicker';
+
+const DateIcon = (style) => (
+    <Icon {...style} name='calendar' />
+)
 
 const combineDateTime = (date, time) => {
     const datePick = moment(date).format('DD MMM YYYY ')
@@ -20,18 +23,27 @@ const AddTask = ({submitHandler}) => {
     const [showTimePicker, setShowTimePicker] = useState(false)
     const [date, setDate] = useState(new Date(Date.now()))
     const [time, setTime] = useState(new Date(Date.now()))
-    const [dateTime, setDateTime] = useState(Date.now())
 
-    const onDateTimeHandler = (dateTime) => {
-        setDateTime(dateTime)
-        console.log(moment(dateTime))
-    }
+
+    const onChangeDate = (event, selectedDate) => {
+        const currentDate = selectedDate || date;
+        setShowDatePicker(false);
+        setDate(currentDate);
+    };
+
+    const onChangeTime = (event, selectedTime) => {
+        const currentTime = selectedTime || time;
+        setShowTimePicker(false);
+        setTime(currentTime);
+    };
 
     const taskData = {
         name: name,
         description: desc,
-        dateTime: dateTime
+        dateTime: combineDateTime(date,time)
     }
+
+    const displayDateTime = moment(combineDateTime(date,time)).format("Do MMMM YYYY, hh:mm:ss a")
 
     return (
         <Layout style={styles.containter}>
@@ -51,12 +63,41 @@ const AddTask = ({submitHandler}) => {
                     placeholder='Description'
                 />
             </View>
-            <View style={styles.inputGroup, { flexDirection: 'row', flex: 1 }}>
-                
-                <CustomDateTimePicker onDateTimeHandler={onDateTimeHandler}/>
+            <View style={styles.inputGroup, { flex: 0.5 }}>
+    <Text style={styles.dateTimeText}>Chosen Time: {displayDateTime}</Text>
+            </View>
+            <View style={styles.inputGroup, { flexDirection: 'row', flex: 1, marginHorizontal: 10 }}>
+                <View style={styles.pickerButton}>
+                    <Button onPress={()=> setShowDatePicker(true)}>Show date picker!</Button>
+                </View>
+                <View style={styles.pickerButton}>
+                    <Button onPress={()=> setShowTimePicker(true)} >Show time picker!</Button>
+                </View>
+                {showDatePicker && (
+                    <DateTimePicker
+                        testID="dateTimePicker"
+                        timeZoneOffsetInMinutes={0}
+                        value={date}
+                        mode={'date'}
+                        is24Hour={true}
+                        display="default"
+                        onChange={onChangeDate}
+                    />
+                )}
+                {showTimePicker && (
+                    <DateTimePicker
+                        testID="dateTimePicker"
+                        timeZoneOffsetInMinutes={0}
+                        value={time}
+                        mode={'time'}
+                        is24Hour={true}
+                        display="default"
+                        onChange={onChangeTime}
+                    />
+                )}
             </View>
             <View style={{ paddingTop: 8, flex: 1 }}>
-                <Button style={styles.button} onPress={() => submitHandler(taskData)}>Add</Button>
+                <Button style={styles.submitButton} onPress={() => submitHandler(taskData)}>Add</Button>
             </View>
 
         </Layout>
@@ -71,10 +112,11 @@ const styles = StyleSheet.create({
 
     },
     input: {
+        marginVertical: 3,
         marginHorizontal: 15,
         flex: 1
     },
-    button: {
+    submitButton: {
         justifyContent: 'center',
         margin: 8,
         backgroundColor: '#1E262C',
@@ -84,6 +126,16 @@ const styles = StyleSheet.create({
         width: '100%',
         paddingBottom: 8,
         position: 'relative'
+    },
+    dateTimeText:{
+        fontSize: 16,
+        fontWeight: 'normal',
+        fontFamily: 'Lato-Regular',
+        marginLeft: 15
+    },
+    pickerButton: {
+        flex: 1, 
+        margin: 3
     }
 })
 
