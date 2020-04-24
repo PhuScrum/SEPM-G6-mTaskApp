d7d1e5dd3c033ea952aa3ea608f2670f82ae28a4
Justin Luu <phuscrum@Justins-MacBook-Pro.local>
2020-04-24 22:17:35 +0700

    *tbc* tagging membes

--- mTaskApp/src/components/tag_members/TagMemberInput.jsx
+++ mTaskApp/src/components/tag_members/TagMemberInput.jsx
@@ -1,72 +1,81 @@
-import React, { Component } from 'react';
-import { View } from 'react-native';
+import React, { Component, useState } from 'react';
+import { View, Text } from 'react-native';
 import SectionedMultiSelect from 'react-native-sectioned-multi-select';
-
+import axios from 'axios'
+import * as url from '../../constants/url/url'
 const items = [
-  // this is the parent or 'item'
-  {
-    name: 'Members',
-    id: 0,
-    // these are the children or 'sub items'
-    children: [
+  {childr: [
       {
-        name: 'Apple',
-        id: 10,
+        name: 'Justin',
+        id: '0',
       },
       {
-        name: 'Strawberry',
-        id: 17,
+        name: 'Tu Bui',
+        id: '1',
       },
-      {
-        name: 'Pineapple',
-        id: 13,
-      },
-      {
-        name: 'Banana',
-        id: 14,
-      },
-      {
-        name: 'Watermelon',
-        id: 15,
-      },
-      {
-        name: 'Kiwi fruit',
-        id: 16,
-      },
-    ],
-  },
+   
+    ]
+    }
+ 
 
 
 ];
 
-export default class TagMemberInput extends Component {
-  constructor() {
-    super();
-    this.state = {
-      selectedItems: [],
-    };
-  }
-  onSelectedItemsChange = (selectedItems) => {
-    this.setState({ selectedItems });
-  };
+export default function TagMemberInput() {
+    const [selectedItems, setSelectedItems] = React.useState([])
+    const [searchResult, setSearchResult] = React.useState(items)
+    const searchMembers = async (searchTerm)=>{
+        var resp = await axios.post(url.searchMembers, {searchTerm})
+        var data = resp.data
+        // console.log(searchTerm, 'search result: ', data)
+        data = restructureData(data)
+        loadToSearchResult(data)
 
-  render() {
+    }
+    const loadToSearchResult =(data)=>{
+        var fake = [
+            {name: 'hoang long',id: 'asdfasklfi'},
+            {name: 'vu pham', id: 'fadsf413423'}
+          ]
+        console.log('fake: ',fake)
+        console.log('data: ', data)
+        
+        items[0]['children'] = fake
+        console.log('loadToSearchResult: ', items)
+        setSearchResult(items)
+
+    }
+    const restructureData = (data)=>{
+        data.map((unit)=>{
+            unit.id = unit._id
+            unit.name = unit.fName + ' ' + unit.lName
+            delete unit._id
+            delete unit.fName
+            delete unit.lName
+            delete unit.email
+            return unit
+        })
+        console.log('restructure data: ', data)
+        return data
+    }
     return (
       <View>
+        <Text>Tag members</Text>
         <SectionedMultiSelect
-          items={items}
+          items={searchResult}
           uniqueKey="id"
           subKey="children"
           selectText="Choose some things..."
           showDropDowns={true}
           readOnlyHeadings={true}
-          onSelectedItemsChange={this.onSelectedItemsChange}
-          selectedItems={this.state.selectedItems}
-          searchPlaceholderText='Search members...'
-          selectText='hello'
-          searchAdornment={(searchTerm)=>{console.log(searchTerm)}}
+          onSelectedItemsChange={(selectedItems)=> setSelectedItems(selectedItems)}
+          selectedItems={selectedItems}
+          searchPlaceholderText='Search members1...'
+          selectText='Tag members'
+        //   searchAdornment={(searchTerm)=>{searchMembers(searchTerm)}}
+        //   filterItems={(searchTerm, items, props)=> {searchMembers(searchTerm)}}
         />
       </View>
     );
-  }
+  
 }
---
+++ mTaskApp/src/constants/global_variables/global-variables.js
@@ -0,0 +1,7 @@
+const localHost = 'http://192.168.1.7:19003/'
+
+const globalVar = {
+    urlDomain: localHost
+}
+
+module.exports = globalVar
