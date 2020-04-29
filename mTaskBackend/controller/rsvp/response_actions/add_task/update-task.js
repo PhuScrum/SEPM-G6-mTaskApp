f6976d057ad86414e069ede34129a0c95eab0dc9
Phu Luu <s3686970@rmit.edu.vn>
2020-04-29 23:54:35 +0700

    *add* handle accept and decline rsvp add task

--- mTaskBackend/controller/rsvp/response_actions/add_task/update-task.js
+++ mTaskBackend/controller/rsvp/response_actions/add_task/update-task.js
@@ -19,26 +19,5 @@ const updateTask = (req, type)=>{
 module.exports = updateTask
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     // req.body.taskId = '5ea96bb938802413a83c6e11'
     // req.body.userId = '5ea59f3ad636b2061cb01778'
