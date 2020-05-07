0f4693fa4415526e3f113c4bd4c5b30e7c91a948
Phu Luu <s3686970@rmit.edu.vn>
2020-05-07 15:28:46 +0700

    *fix* response type in send notification

--- mTaskApp/src/constants/global_variables/global-variables.js
+++ mTaskApp/src/constants/global_variables/global-variables.js
@@ -1,4 +1,4 @@
-const justinLocalhost = 'http://192.168.1.7:19003/'
+const justinLocalhost = 'http://192.168.1.10:19003/'
 const urlDomain = 'https://bigquery-project-medium.df.r.appspot.com/'
 
 const globalVar = {
--- mTaskBackend/controller/rsvp/response_actions/add_task/accept.js
+++ mTaskBackend/controller/rsvp/response_actions/add_task/accept.js
@@ -4,7 +4,7 @@ const notifyCreator = require('./notify-creator')
 const accept = (req, res)=>{
     updateTask(req, 'accept') // taskId, userId
     updateRSVP(req, {isAccepted: true}) // rsvpId
-    notifyCreator(req) //creatorId
+    notifyCreator(req, 'accept') //creatorId
     res.json('done')
 }
 
