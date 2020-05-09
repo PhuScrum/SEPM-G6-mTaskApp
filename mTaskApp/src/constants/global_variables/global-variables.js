fa1a0acf1394af95a440da6aca64c475312a885b
anhtubui1204 <s3634162@rmit.edu.vn>
2020-05-09 19:03:31 +0700

    adjust post list

--- mTaskApp/src/constants/global_variables/global-variables.js
+++ mTaskApp/src/constants/global_variables/global-variables.js
@@ -1,9 +1,10 @@
 const justinLocalhost = 'http://192.168.0.106:19003/'
 const cloudURL = 'https://bigquery-project-medium.df.r.appspot.com/'
 const tuIP = 'http://192.168.0.106:19003/'
+const huongIP = 'http://192.168.1.238:19003/'
 
 const globalVar = {
-    urlDomain: tuIP
+    urlDomain: huongIP
 }
 
 module.exports = globalVar
--- mTaskBackend/controller/list/crud/post-list.js
+++ mTaskBackend/controller/list/crud/post-list.js
@@ -1,5 +1,16 @@
 const listModel = require('../../../model/list')
+const mongoose = require('mongoose')
+
+String.prototype.toObjectId = function () {
+    var ObjectId = (mongoose.Types.ObjectId);
+    return new ObjectId(this.toString());
+};
+
+
 const postList = (req, res)=>{
+
+    req.body.creatorId = req.body.creatorId.toObjectId()
+
     listModel.create(req.body, (err, doc)=>{
         if(err)console.log(err)
         else res.json(doc)
